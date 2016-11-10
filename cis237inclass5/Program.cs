9fb6773c333e22a0b939be8e36e0e9d1a7f35a77
Alyssa Mahler <amstrand21@gmail.com>
2016-11-10 17:42:56 -0500

    Adds query functionality.

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -31,13 +31,31 @@ namespace cis237inclass5
             Console.WriteLine("Print out a found car using the Find method");
             Console.WriteLine();
             Console.WriteLine(foundCar.id + " " + foundCar.make + " " + foundCar.model);
+            Console.WriteLine();
 
                 // ********************************************************************************************
                 // Find a specific car by any property:
                 // Call the Where method on the table Cars and pass in a lambda expression
                 // for the criteria we're looking for. This loops through the DB and runs 
                 // the expression against each of them - when true, it returns that car.
+                // SQL equivalent:      Select * From Cars Where id = "V0LCD1814" Limit 1
             Car carToFind = carsEntities.Cars.Where(car => car.id == "V0LCD1814").First();
+            Console.WriteLine("Find a car by ID");
+            Console.WriteLine();
+            Console.WriteLine(carToFind.id + " " + carToFind.make + " " + carToFind.model);
+            Console.WriteLine();
+
+                // To find multiple objects by a shared property and save as a list:
+            List<Car> foundCars = carsEntities.Cars.Where(car => car.cylinders == 8).ToList<Car>();
+                // To output each car in the list:
+            Console.WriteLine();
+            Console.WriteLine("To find all cars in the list that have 8 cylinders");
+            Console.WriteLine();
+            foreach (Car car in foundCars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
+            Console.WriteLine();
 
 
 
