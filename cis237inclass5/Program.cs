9ca7304819a641cd7b70917940ad70a0c72ec014
Alyssa Mahler <amstrand21@gmail.com>
2016-11-15 17:26:35 -0500

    Adds database functionalities.

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -15,7 +15,7 @@ namespace cis237inclass5
 
                 // ********************************************************************************************
                 // List all the cars in the table:
-            Console.WriteLine("Print the list");
+            Console.WriteLine("Print the full database list of cars");
             Console.WriteLine();
             foreach (Car car in carsEntities.Cars)
             {
@@ -28,7 +28,7 @@ namespace cis237inclass5
 
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Print out a found car using the Find method");
+            Console.WriteLine("Print out a specific car using the Find method");
             Console.WriteLine();
             Console.WriteLine(foundCar.id + " " + foundCar.make + " " + foundCar.model);
             Console.WriteLine();
@@ -40,7 +40,7 @@ namespace cis237inclass5
                 // the expression against each of them - when true, it returns that car.
                 // SQL equivalent:      Select * From Cars Where id = "V0LCD1814" Limit 1
             Car carToFind = carsEntities.Cars.Where(car => car.id == "V0LCD1814").First();
-            Console.WriteLine("Find a car by ID");
+            Console.WriteLine("Find a car by ID using the Where method");
             Console.WriteLine();
             Console.WriteLine(carToFind.id + " " + carToFind.make + " " + carToFind.model);
             Console.WriteLine();
@@ -57,7 +57,90 @@ namespace cis237inclass5
             }
             Console.WriteLine();
 
+                // ********************************************************************************************
+                // Add a new car to the database:
 
+                // Make an instance of a new Car:
+            Car newCarToAdd = new Car();
+                // Add properties to the new car:
+            newCarToAdd.id = "88888";
+            newCarToAdd.make = "Nissan";
+            newCarToAdd.model = "GT-R";
+            newCarToAdd.horsepower = 550;
+            newCarToAdd.cylinders = 8;
+            newCarToAdd.year = "2016";
+            newCarToAdd.type = "";
+
+            try
+            {
+                    // Add the new car to the cars collection:
+                carsEntities.Cars.Add(newCarToAdd);
+
+                    // Persist the collection to the database:
+                carsEntities.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                    // Remove the new car from the collection since we can't save it:
+                carsEntities.Cars.Remove(newCarToAdd);
+                    // Output error to user:
+                Console.WriteLine("The record cannot be added: " + ex.Message);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Just added a new car.  Going to fetch and print to verify.");
+
+            carToFind = carsEntities.Cars.Find("88888");
+            Console.WriteLine(carToFind.id + " " + carToFind.make + " " + carToFind.model);
+
+            Console.WriteLine();
+
+                // ********************************************************************************************
+                // Update a record:
+
+                // Get the car to update:
+            Car carToFindForUpdate = carsEntities.Cars.Find("88888");
+
+            Console.WriteLine();
+            Console.WriteLine("Update car #88888:");
+
+                // Update some of the properties:
+            carToFindForUpdate.make = "Nisssssssssan";
+            carToFindForUpdate.model = "GT-RRRRR";
+
+            Console.WriteLine("88888 Nissan GT-R is now " + carToFindForUpdate.id + " " + carToFindForUpdate.make + " " + carToFindForUpdate.model);
+            Console.WriteLine();
+
+                // Save the changes to the database:
+            carsEntities.SaveChanges();
+
+                // ********************************************************************************************
+                // Delete a car from the database:
+
+                // Get a car out of the database that we would like to delete:
+            Car carToFindForDelete = carsEntities.Cars.Find("88888");
+
+                // Remove the car from the collection:
+            carsEntities.Cars.Remove(carToFindForDelete);
+
+                // Save the changes to the database:
+            carsEntities.SaveChanges();
+
+            Console.WriteLine("We deleted that car.  Check to see the car was deleted:");
+            Console.WriteLine();
+
+                // Check to see if the car was deleted:
+            try
+            {
+                carToFindForDelete = carsEntities.Cars.Find("88888");
+                Console.WriteLine(carToFindForDelete.id + " " + carToFindForDelete.make + " " + carToFindForDelete.model);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("That vehicle does not exist: " + ex.Message);
+            }
+
+           
 
         }
     }
